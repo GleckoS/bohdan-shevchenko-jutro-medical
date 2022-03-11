b0d961ac3dd8b7767b4292bc550650247a0f114a
Glecko <shevabogdan16@gmail.com>
2022-03-11 18:51:37 +0100

    optimisation

--- components/atoms/icon.jsx
+++ components/atoms/icon.jsx
@@ -1,7 +1,7 @@
 import react from "react";
 
 const Icon = ({ icon, classes }) => (
-    <img className={'transition mb-[56px] w-fit m:max-h-[80px] m:w-auto ' + classes} src={icon} />
+    <img className={'transition mb-[56px] w-fit m:max-h-[80px] m:w-auto ' + classes} src={icon} alt='' />
 )
 
 export default Icon
--- components/atoms/iphoneScreen.jsx
+++ components/atoms/iphoneScreen.jsx
@@ -1,7 +1,7 @@
 import react from "react";
 
 const IphoneScreen = ({ img, classes }) => (
-    <img className={'w-full transition-opacity 2xlm:absolute ' + classes} src={img} />
+    <img className={'w-full transition-opacity 2xlm:absolute ' + classes} src={img} alt=''/>
 )
 
 export default IphoneScreen
